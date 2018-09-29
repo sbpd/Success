94214f75c40ba9079ee5f1792fce3c43d99ffa2c
sbajaj5 <30503794+sbajaj5@users.noreply.github.com>
2018-09-29 16:53:22 -0400

    .

---
+++ basics/boolean.js
@@ -0,0 +1,18 @@
+// === - equality operator
+// !== - no equal operator
+// < - less than operator
+// > - greater than operator
+// <= - less than or equal to operator
+// >= - greater than or equal to operator
+
+let temp = 32
+let isFreezing = temp >= 32
+
+console.log(isFreezing)
+
+let age = 1
+let isChild = age <= 7
+let isSenior = age >= 65
+
+console.log(isChild)
+console.log(isSenior)
