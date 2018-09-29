9c713f7a07059230d39a4c5cbc103f79f947c48c
sbajaj5 <30503794+sbajaj5@users.noreply.github.com>
2018-09-29 14:11:49 -0400

    2nd commit

---
+++ basics/numbers.js
@@ -0,0 +1,10 @@
+let age = 26;
+let dogYears = (age + 1) / 7
+
+console.log(dogYears);
+
+let studentScore = 41
+let maxscore = 100
+let percent = (studentScore / maxscore) * 100
+
+console.log(percent)
