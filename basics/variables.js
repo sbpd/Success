79a1c294e96202d4ad13eec947740a5580ec40bf
sbajaj5 <30503794+sbajaj5@users.noreply.github.com>
2018-09-29 14:20:35 -0400

    3rd commit

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+// You can't define a variable more than once
+
+let  petName = 'Lalu'
+
+petName = 'Spot'
+
+console.log(petName)
+
+// There are rules related to variable names
+/**
+ * The first letter in variable needs to start a-z, $ or _. It can't start with a nuumber
+ * Variable names cannot be reserved keywords
+ */
+
+ 
