83b3daf8a0629eebd1e613da6b8ff38c2729891d
sbajaj5 <30503794+sbajaj5@users.noreply.github.com>
2018-09-29 17:06:54 -0400

    0

--- basics/boolean.js
+++ basics/boolean.js
@@ -5,14 +5,23 @@
 // <= - less than or equal to operator
 // >= - greater than or equal to operator
 
-let temp = 32
-let isFreezing = temp >= 32
+let temp = 180
 
-console.log(isFreezing)
+if (temp <= 32) {
+  console.log('It is freezing outside!')
+}
 
-let age = 1
-let isChild = age <= 7
-let isSenior = age >= 65
+if (temp >= 110){
+  console.log('It is way too hot outside!')
+  console.log('Testing123')
+}
 
-console.log(isChild)
-console.log(isSenior)
+let age = 66
+
+if(age <= 7) {
+  console.log('This child is eligible for discount')
+}
+
+if (age >= 65) {
+  console.log('You are eligible for senior discount')
+}
