3cf25ff362d429450db3e4a51107bd5fe300e7a1
sbajaj5 <30503794+sbajaj5@users.noreply.github.com>
2018-09-29 13:58:55 -0400

    firstCommit

---
+++ basics/strings.js
@@ -0,0 +1,10 @@
+let firstName = 'Sumeet'
+let lastName = 'Bajaj'
+let fullName = firstName + " " + lastName
+
+console.log(fullName)
+
+let city = "Boston"
+let country = "United States"
+
+console.log(city + ', ' + country)
