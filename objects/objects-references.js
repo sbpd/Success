80bb31f9939b92ec5f8ae162850ff9f282bf0119
sbajaj5 <30503794+sbajaj5@users.noreply.github.com>
2018-10-10 07:39:27 -0400

    9+

---
+++ objects/objects-references.js
@@ -0,0 +1,15 @@
+let myAccount= {
+  name: "Sumeet Bajaj",
+  expenses: 0, 
+  income: 0
+}
+
+let addExpense = function (account, amount) {
+  account = {}
+  //account.expenses = account.expenses + amount
+  console.log(account)
+}
+
+addExpense(myAccount, 2.5)
+
+console.log(myAccount)
