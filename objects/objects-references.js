92513fd156750f45ae6c8cc98c44cac7d9084b34
sbajaj5 <30503794+sbajaj5@users.noreply.github.com>
2018-10-10 07:58:47 -0400

    6+

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -4,12 +4,27 @@ let myAccount= {
   income: 0
 }
 
-let addExpense = function (account, amount) {
-  account = {}
-  //account.expenses = account.expenses + amount
-  console.log(account)
+let addIncome = function(account, income) {
+  account.income = account.income + income
 }
 
-addExpense(myAccount, 2.5)
+let addExpense = function(account, expense) {
+  account.expenses = account.expenses + expense
+}
 
-console.log(myAccount)
+let resetAccount = function (account) {
+  account.expenses = 0
+  account.income = 0
+}
+let getAccountSummary = function(account) {
+  let balance = account.income - account.expenses
+  return `Account for ${account.name} has $${balance}. $${account.income} in income and $${account.expenses} in expenses`
+}
+
+addIncome(myAccount, 2000)
+addExpense(myAccount, 2.5)
+addExpense(myAccount, 160)
+console.log(getAccountSummary(myAccount))
+
+resetAccount(myAccount)
+console.log(getAccountSummary(myAccount))
