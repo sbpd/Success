c24ba9cea9d73bd4e68217e6641fef4a1e1a37ad
sbajaj5 <30503794+sbajaj5@users.noreply.github.com>
2018-10-10 07:26:31 -0400

    65

---
+++ objects/objects-functions.js
@@ -0,0 +1,39 @@
+let myBook = {
+  title: "1984",
+  author: "George Orwell",
+  pageCount: 326  
+}
+
+let otherBook = {
+  title: "A Peoples History",
+  author: "Howard Zinn",
+  pageCount: 723  
+}
+
+let getSummary = function (book) {
+  return {
+    summary: `${book.title} by ${book.author}`,
+    pageCountSummary: `${book.title} is ${book.pageCount} pages long`
+  }
+}
+
+let bookSummary = getSummary(myBook)
+let otherBookSummary = getSummary(otherBook)
+
+console.log(bookSummary.pageCountSummary)
+
+// Challenge area
+
+let conversionTemp = function (farenheit) {
+  let tempConvert = {
+    fToC: (farenheit - 32) / (9/5),
+    fToK: (farenheit + 459.67) * 5 / 9
+  }
+
+  return {
+    answer: `${farenheit} farenheit is converted to ${tempConvert.fToC} celcius and ${tempConvert.fToK} Kelvin`
+  }
+ }
+
+ let result = conversionTemp(32)
+ console.log(result.answer)
